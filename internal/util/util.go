2ea31fc537c3edbc22577246aabd9d70f817d618
le0nchw <105686548+le0nchw@users.noreply.github.com>
2023-08-18 11:16:44 +0800

    perf: use std base64 url decode

--- internal/util/util.go
+++ internal/util/util.go
@@ -2,21 +2,9 @@ package util
 
 import (
 	"encoding/base64"
-	"strings"
 )
 
 // Decode base64 URL-encoded data
 func DecodeBase64URL(data string) ([]byte, error) {
-	// Convert base64 URL encoded string to standard base64 encoding
-	data = strings.ReplaceAll(data, "-", "+")
-	data = strings.ReplaceAll(data, "_", "/")
-
-	// Add padding if necessary
-	pad := len(data) % 4
-	if pad != 0 {
-		data += strings.Repeat("=", 4-pad)
-	}
-
-	// Decode base64 data
-	return base64.StdEncoding.DecodeString(data)
+	return base64.URLEncoding.DecodeString(data)
 }
